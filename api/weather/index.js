66a63a9c90b05ea34716e16875e7beb79e28d9c8
kiingdavies <daviesadedeji007@gmail.com>
2021-09-02 15:22:37 +0100

    initial commit

--- api/weather/index.js
+++ api/weather/index.js
@@ -1,12 +1,20 @@
+//const AZURE_MAPS_KEY = require('AZURE_MAPS_KEY').config({path: 'C:\Than-StaticWebApp\swa-weather-react\azure-maps-key.js'});
+// const dotenv = require('dotenv').config();
+// require('dotenv-azure')
+
+
 const axios = require('axios');
+
+// console.log(dotenv);
 
 module.exports = async function (context, req) {
     const coordinates = req.query.coordinates || "49.2833329,-123.1200278";
 
-    const endpoint = 'https://atlas.microsoft.com/weather/currentConditions/json';
+    const endpoint = 'https://atlas.microsoft.com/weather/currentConditions/json?api-version=1.0&query=47.60357,-122.32945&subscription-key=wXktRsYH31CuBD87mxJhMAqi1u_WqDn8uZtXkb6zMSo';
+    // const endpoint = 'https://atlas.microsoft.com/weather/currentConditions/json';
 
     const params = {
-        'subscription-key': process.env.AZURE_MAPS_KEY,
+        // 'subscription-key': process.env.AZURE_MAPS_KEY,
         'api-version': '1.0',
         'query': coordinates,
     };
