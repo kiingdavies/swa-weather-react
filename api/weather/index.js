bdc4939b4889195213ba2c57ae9a05df6df11d06
kiingdavies <daviesadedeji007@gmail.com>
2021-09-07 09:16:10 +0100

    Add API

--- api/weather/index.js
+++ api/weather/index.js
@@ -2,8 +2,8 @@
 // const dotenv = require('dotenv').config();
 // require('dotenv-azure')
 
-
 const axios = require('axios');
+// const {AZURE_MAPS_KEY} = process.env;
 
 // console.log(dotenv);
 
@@ -11,7 +11,8 @@ module.exports = async function (context, req) {
     const coordinates = req.query.coordinates || "49.2833329,-123.1200278";
 
     const endpoint = 'https://atlas.microsoft.com/weather/currentConditions/json?api-version=1.0&query=47.60357,-122.32945&subscription-key=wXktRsYH31CuBD87mxJhMAqi1u_WqDn8uZtXkb6zMSo';
-    // const endpoint = 'https://atlas.microsoft.com/weather/currentConditions/json';
+    //  const endpoint = `https://atlas.microsoft.com/weather/currentConditions/json?api-version=1.0&query=47.60357,-122.32945&subscription-key=${AZURE_MAPS_KEY}`;
+    // const endpoint = `https://atlas.microsoft.com/weather/currentConditions/json`;
 
     const params = {
         // 'subscription-key': process.env.AZURE_MAPS_KEY,
